f911f81ca49e3adb7ae9d018257a449d51299bfd
trodriguez-png <trodriguez@datservicesinc.com>
2025-12-03 10:24:50 -0500

    fixed rotation

--- main.py
+++ main.py
@@ -16,15 +16,16 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        dt = (clock.tick(60) / 1000)
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = (clock.tick(60) / 1000)
-
+        
 pygame.quit()
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 from circleshape import CircleShape
 
@@ -17,3 +17,17 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)    
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+
